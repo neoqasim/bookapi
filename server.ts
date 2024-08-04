96f52e9083416158e74091dde8b2492b64d90416
neoqasim <neoqasim@gmail.com>
2024-08-04 11:30:16 -0700

    created a get request

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
-console.log('working');
+
+import app from "./src/app";
+
+const startServer = () => {
+    const port = process.env.PORT || 3001
+
+
+
+
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    })
+}
+
+startServer()
